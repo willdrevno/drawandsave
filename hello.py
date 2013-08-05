5ef7651656eee5558859c66f053b60f989d2f63a
willdrevno <wdrevno@gmail.com>
2013-08-04 23:40:41 -0700

    static files

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG = True,
+)
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
